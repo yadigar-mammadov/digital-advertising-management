1cbf4f0e38521c220ed2898716cd9222c72debde
devYadigar <dev.yadigar@gmail.com>
2026-09-08 03:39:20 +0400

    chore: add Spring Boot application bootstrap

---
+++ src/main/java/com/digitaladvertisingmanagement/DigitalAdvertisingManagementApplication.java
@@ -0,0 +1,12 @@
+package com.digitaladvertisingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAdvertisingManagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DigitalAdvertisingManagementApplication.class, args);
+  }
+}
